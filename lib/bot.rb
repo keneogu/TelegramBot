3c3d55ea58d3f98d80ce9024a77ffc6bab1d586e
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-01-28 12:32:36 +0100

    Modified bot.rb file

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,4 +1,5 @@
 # rubocop: disable Metrics/MethodLength
+# rubocop: disable Metrics/AbcSize
 require 'telegram/bot'
 require_relative 'content'
 
@@ -8,48 +9,45 @@ class Bot
   def initialize
     @token = '1583152588:AAHSFkxUfbonb-w-R58XrBbbfuFFhe2suWg'
     @countries = Countries.new
-    messages
-  end
-end
+    Telegram::Bot::Client.run(token) do |bot|
+      bot.listen do |message|
+        case message.text
 
-def messages
-  Telegram::Bot::Client.run(token) do |bot|
-    bot.listen do |message|
-      case message.text
-
-      when '/start'
-        bot.api.send_message(chat_id: message.chat.id, text:
-        "Hello, #{message.from.first_name}
+        when '/start'
+          bot.api.send_message(chat_id: message.chat.id, text:
+          "Hello, #{message.from.first_name}
 		This is a ruby project created by kene\n
 		Use /start to start the bot\n
 		Use /stop to stop or end the bot\n
 		enter / + country name in west africa to obtain information about the country\n
 		example enter: /Nigeria  /Togo   /Benin  /Senegal")
 
-      when '/Nigeria'
-        @countries.nigeria
-        bot.api.send_message(chat_id: message.chat.id, text: @countries.nigeria, date: message.date)
+        when '/Nigeria'
+          @countries.nigeria
+          bot.api.send_message(chat_id: message.chat.id, text: @countries.nigeria, date: message.date)
 
-      when '/Togo'
-        @countries.togo
-        bot.api.send_message(chat_id: message.chat.id, text: @countries.togo, date: message.date)
+        when '/Togo'
+          @countries.togo
+          bot.api.send_message(chat_id: message.chat.id, text: @countries.togo, date: message.date)
 
-      when '/Benin'
-        @countries.benin
-        bot.api.send_message(chat_id: message.chat.id, text: @countries.benin, date: message.date)
+        when '/Benin'
+          @countries.benin
+          bot.api.send_message(chat_id: message.chat.id, text: @countries.benin, date: message.date)
 
-      when '/Senegal'
-        @countries.senegal
-        bot.api.send_message(chat_id: message.chat.id, text: @countries.senegal, date: message.date)
+        when '/Senegal'
+          @countries.senegal
+          bot.api.send_message(chat_id: message.chat.id, text: @countries.senegal, date: message.date)
 
-      when '/stop'
-        bot.api.send_message(chat_id: message.chat.id, text:
-        "Thanks for accessing, #{message.from.first_name}. Bye!", date: message.date)
+        when '/stop'
+          bot.api.send_message(chat_id: message.chat.id, text:
+          "Thanks for accessing, #{message.from.first_name}. Bye!", date: message.date)
 
-      else
-        bot.api.send_message(chat_id: message.chat.id, text: 'Invalid text! or country not avaliable at the moment')
+        else
+          bot.api.send_message(chat_id: message.chat.id, text: 'Invalid text! or country not avaliable at the moment')
+        end
       end
     end
   end
 end
+# rubocop: enable Metrics/AbcSize
 # rubocop: enable Metrics/MethodLength
