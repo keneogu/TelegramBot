f90c8d437cdffc9f5f17b30b30863ca3d504fb18
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-01-28 14:31:18 +0100

    fixed rubocop and modified bit.rb

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,5 +1,4 @@
 # rubocop: disable Metrics/MethodLength
-# rubocop: disable Metrics/AbcSize
 require 'telegram/bot'
 require_relative 'content'
 
@@ -9,34 +8,27 @@ class Bot
   def initialize
     @token = '1583152588:AAHSFkxUfbonb-w-R58XrBbbfuFFhe2suWg'
     @countries = Countries.new
+    messages
+  end
+
+  def messages
     Telegram::Bot::Client.run(token) do |bot|
       bot.listen do |message|
         case message.text
 
         when '/start'
-          bot.api.send_message(chat_id: message.chat.id, text:
-          "Hello, #{message.from.first_name}
-		This is a ruby project created by kene\n
-		Use /start to start the bot\n
-		Use /stop to stop or end the bot\n
-		enter / + country name in west africa to obtain information about the country\n
-		example enter: /Nigeria  /Togo   /Benin  /Senegal")
-
+          bot.api.send_message(chat_id: message.chat.id, text: welcome_text(message))
         when '/Nigeria'
-          @countries.nigeria
-          bot.api.send_message(chat_id: message.chat.id, text: @countries.nigeria, date: message.date)
+          country_selector('nigeria', bot, message)
 
         when '/Togo'
-          @countries.togo
-          bot.api.send_message(chat_id: message.chat.id, text: @countries.togo, date: message.date)
+          country_selector('togo', bot, message)
 
         when '/Benin'
-          @countries.benin
-          bot.api.send_message(chat_id: message.chat.id, text: @countries.benin, date: message.date)
+          country_selector('benin', bot, message)
 
         when '/Senegal'
-          @countries.senegal
-          bot.api.send_message(chat_id: message.chat.id, text: @countries.senegal, date: message.date)
+          country_selector('senegal', bot, message)
 
         when '/stop'
           bot.api.send_message(chat_id: message.chat.id, text:
@@ -48,6 +40,19 @@ class Bot
       end
     end
   end
+
+  def welcome_text(message)
+    "Hello, #{message.from.first_name}
+		This is a ruby project created by kene\n
+		Use /start to start the bot\n
+		Use /stop to stop or end the bot\n
+		enter / + country name in west africa to obtain information about the country\n
+    example enter: /Nigeria  /Togo   /Benin  /Senegal"
+  end
+
+  def country_selector(country, bot, message)
+    @countries.send(country)
+    bot.api.send_message(chat_id: message.chat.id, text: @countries.send(country), date: message.date)
+  end
 end
-# rubocop: enable Metrics/AbcSize
 # rubocop: enable Metrics/MethodLength
