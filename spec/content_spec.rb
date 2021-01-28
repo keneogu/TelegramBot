a0e9cdcc243909c323177d91487dac1c71afa701
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-01-28 15:39:53 +0100

    Add spec test

--- spec/content_spec.rb
+++ spec/content_spec.rb
@@ -6,5 +6,14 @@ describe Countries do
     it 'displays information about Nigeria' do
       expect(country.nigeria).to match(/s/)
     end
+    it 'displays information about togo' do
+      expect(country.nigeria).to be
+      "Togo, (French: République togolaise), \n
+		is a country in West Africa on the Gulf of Guinea, \n
+		is known for its palm-lined beaches and hilltop villages. \n
+		bordered by Ghana to the west, Benin to the east and Burkina Faso to the north \n
+		Capital: Lome
+		Official languages: French"
+    end
   end
 end
